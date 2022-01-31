8cb469f314d9955f497c0d0d0b180347b19edc24
Izuho Fujiwara <fujishan2212@gmail.com>
2022-02-01 06:40:38 +0900

    fix

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -3,7 +3,7 @@ import { getConfig } from "./config";
 const env = getConfig("ENV");
 
 export const isProduction = (): boolean => {
-	return env === "production" || env == "prod";
+	return env === "production" || env === "prod";
 };
 
 export const isDevelopment = (): boolean => {
