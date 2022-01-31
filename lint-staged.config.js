6a6956e4967024bc5d673355b9fdc29387a26b9e
Izuho Fujiwara <fujishan2212@gmail.com>
2022-02-01 07:10:06 +0900

    add eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,6 @@ module.exports = {
 		`tsc --showConfig -p tsconfig.json | \
         jq -j ".compilerOptions | to_entries | map(\\"--\\" + .key + \\" \\" + (.value | if type==\\"array\\" then join(\\",\\") else . end | tostring)) | @sh" | \
         xargs -t -I {} sh -c 'tsc --noEmit {} ${fileNames.join(" ")}'`,
+		`eslint ${fileNames.join(" ")}`,
 	],
 };
