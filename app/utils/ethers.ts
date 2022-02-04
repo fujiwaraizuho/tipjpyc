60b7a1db810c0698ef6a29028e71209dc3736742
Izuho Fujiwara <fujishan2212@gmail.com>
2022-02-05 05:31:10 +0900

    add comment

--- app/utils/ethers.ts
+++ app/utils/ethers.ts
@@ -5,5 +5,6 @@ export const getDepositAddress = (userId: number): string => {
 	const masterXpub = getConfig("ETHERS_MASTER_XPUB");
 	const node = ethers.utils.HDNode.fromExtendedKey(masterXpub);
 
+	// m/44'/60'/0'/{user_id}
 	return node.derivePath(userId.toString()).address;
 };
