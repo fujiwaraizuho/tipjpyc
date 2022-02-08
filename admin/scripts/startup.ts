a2fa49caf91d46c4e1c1cc5622f6259e0d152af6
Izuho Fujiwara <fujishan2212@gmail.com>
2022-02-08 19:15:34 +0900

    first startup script

---
+++ admin/scripts/startup.ts
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+
+const main = async () => {
+	const password = "";
+
+	console.log(`Password: \n${password}\n`);
+
+	const entropy = ethers.utils.randomBytes(32);
+	const mnemonicPhrase = ethers.utils.entropyToMnemonic(entropy);
+
+	console.log(`MnemonicPhrase: \n${mnemonicPhrase}\n`);
+
+	const privateNode = ethers.utils.HDNode.fromMnemonic(
+		mnemonicPhrase,
+		password
+	);
+	const publicNode = privateNode.derivePath("m/44'/60'/0'").neuter();
+
+	const xpub = publicNode.extendedKey;
+
+	console.log(`DepositAddress XPUB (m/44'/60'/0'): \n${xpub}\n`);
+
+	const firstUserAddress = publicNode.derivePath("1").address;
+
+	console.log(`FirstUserAddress (m/44'/60'/0'/1): \n${firstUserAddress}\n`);
+
+	const adminAddress = privateNode.derivePath("m/44'/60'/1'/0").address;
+	console.log(`AdminAddress (m/44'/60'/1'/0): \n${adminAddress}`);
+};
+
+main();
