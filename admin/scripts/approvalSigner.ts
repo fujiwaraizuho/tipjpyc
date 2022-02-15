584fb10821ec972419138aaa74ca1e630edd0657
Yosuke-Aramaki <gnx.vw903@gmail.com>
2022-02-15 20:32:17 +0900

    check lint

--- admin/scripts/approvalSigner.ts
+++ admin/scripts/approvalSigner.ts
@@ -21,6 +21,61 @@ const main = async () => {
 		ALCHEMY_API_KEY
 	);
 	const signer = new LedgerSigner(provider, "hid");
+
+	const adminAddress = await signer.getAddress(PATH);
+	console.info(`-> AdminAddress: ${adminAddress} (${PATH})`);
+
+	const jpycV1Contract = new ethers.Contract(
+		getConfig("JPYC_CONTRACT_ADDRESS"),
+		jpycV1Abi
+	);
+	console.info(`-> Approval from ${NUMBER_OF_USER} deposit address.`);
+	console.info("--------------------");
+
+	for (let i = 1; i <= NUMBER_OF_USER; i++) {
+		const userPath = `m/44'/60'/0'/${i}`;
+		const userAddress = await signer.getAddress(userPath);
+
+		console.info(`-> Start pproval: ${i}/${NUMBER_OF_USER}`);
+		console.info(`[${i}] Address = ${userAddress}`);
+
+		const tx = await jpycV1Contract.populateTransaction.approve(
+			adminAddress,
+			ethers.utils.parseUnits(APPROVAL_AMOUNT, 18)
+		);
+
+		tx.chainId = NETWORK_ID;
+		tx.gasLimit = ethers.BigNumber.from("300000");
+
+		console.info("> Ledger でトランザクションに署名してください");
+
+		let sendTx: ethers.providers.TransactionResponse;
+
+		try {
+			const unsignedTx = await signer.signTransaction(tx, userPath);
+			sendTx = await provider.sendTransaction(unsignedTx);
+		} catch (err) {
+			console.error(err);
+			console.info(
+				`> 署名に失敗しました。${i}から処理しなおしてください`
+			);
+			exit();
+		}
+
+		console.info(`[${i}] https://polygonscan.com/tx/${sendTx.hash}`);
+
+		const sentTx = await sendTx.wait(1);
+
+		if (!sentTx.status) {
+			console.error(`[${i}] Failed TX...`);
+			continue;
+		}
+
+		console.info(`[${i}] Success TX!`);
+		console.info("--------------------");
+	}
+
+	console.info("-> Complete Approval from deposit account");
 };
 
 main();
